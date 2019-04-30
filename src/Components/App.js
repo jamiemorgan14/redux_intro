28b59f8c9066c10aa89f2192c38ea34a48df3a8a
Jamie <jmorgan3475@gmail.com>
2019-04-29 22:10:28 -0600

    courses init

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './Home/HomePage.jsx';
 import AboutPage from './About/AboutPage.jsx'
 import Header from './Common/Header'
+import PageNotFound from './PageNotFound';
+import CoursesPage from './Courses/CoursesPage';
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   )
 }
--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -8,10 +8,12 @@ const Header = () => {
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
       {" | "}
-      <NavLink to="/about" activeStyle={activeStyle} exact>About</NavLink>
+      <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
 
     </nav>
   )
 }
 
-export default Header
+export default Header;
