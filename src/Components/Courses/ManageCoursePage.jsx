c0800753d153eada78885b3accf968fad9cbb9d3
Jamie <jmorgan3475@gmail.com>
2019-05-01 15:45:09 -0600

    form reloads with data on refresh

--- src/Components/Courses/ManageCoursePage.jsx
+++ src/Components/Courses/ManageCoursePage.jsx
@@ -21,6 +21,8 @@ function ManageCoursesPage({
       loadCourses().catch(error => {
         alert("Loading courses failed" + error)
       });
+    } else {
+      setCourse({ ...props.course })
     }
 
     if (authors.length === 0) {
@@ -28,7 +30,7 @@ function ManageCoursesPage({
         alert("Loading authors failed" + error)
       });
     }
-  }, []);
+  }, [props.course]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -64,9 +66,15 @@ ManageCoursesPage.propTypes = {
   history: propTypes.object.isRequired
 }
 
-function mapStateToProps(state) {
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course = slug && state.courses.length > 0 ? getCourseBySlug(state.courses, slug) : newCourse;
   return {
-    course: newCourse,
+    course,
     courses: state.courses,
     authors: state.authors
   }
