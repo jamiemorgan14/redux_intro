29bb2a71a0b6608c0444066c28c0e596687bbec3
Jamie <jmorgan3475@gmail.com>
2019-05-01 12:40:57 -0600

    manage changed from class to function comp

--- src/Components/Courses/ManageCoursePage.jsx
+++ src/Components/Courses/ManageCoursePage.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import * as courseActions from "../../Redux/actions/courseActions";
 import * as authorActions from "../../Redux/actions/authorActions";
 import propTypes from 'prop-types';
 
-class ManageCoursesPage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, actions, loadAuthors, loadCourses } = this.props;
-
+function ManageCoursesPage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
         alert("Loading courses failed" + error)
       });
     }
+
     if (authors.length === 0) {
       loadAuthors().catch(error => {
         alert("Loading authors failed" + error)
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    )
-  }
+  }, [])
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  )
 }
 
 ManageCoursesPage.propTypes = {
