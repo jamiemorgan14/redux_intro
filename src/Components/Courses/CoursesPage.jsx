3bb2f49081de66017c315fc11d60b908563be02b
Jamie <jmorgan3475@gmail.com>
2019-05-01 00:10:58 -0600

    displays whole course object

--- src/Components/Courses/CoursesPage.jsx
+++ src/Components/Courses/CoursesPage.jsx
@@ -3,38 +3,20 @@ import { connect } from 'react-redux'
 import * as courseActions from "../../Redux/actions/courseActions.jsx"
 import propTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from "./CourseList.jsx"
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => {
+      alert("Loading courses failed" + error)
+    })
   }
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course)
-  }
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
+        <CourseList courses={this.props.courses} />
+      </>
     )
   }
 }
